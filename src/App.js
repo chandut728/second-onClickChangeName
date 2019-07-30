a074950ab5768599fcc915da2a528e6d8d094a0b
Chandrakant Thakur <chandut728@gmail.com>
2019-07-30 17:05:54 +0530

    onClickTextChange

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import './App.css'
 
-import React, {Component} from 'react'
+import React, {useState , useEffect} from 'react'
 
-class App extends Component {
-  render() {
-    return <div className="App">
-      <div className="App-heading App-flex">
-        <h2>Welcome to <span className="App-react">React</span></h2>
-      </div>
-      <div className="App-instructions App-flex">
-        <img className="App-logo" src={require('./react.svg')}/>
-        <p>Edit <code>src/App.js</code> and save to hot reload your changes.</p>
-      </div>
-    </div>
+function App(){
+
+  const [ name , setName ] = useState("Chandu")
+  const [ city , setCity ] = useState("Malvan")
+
+  const handleClick = ()=>{
+    setName('Chandrakant')
+    setCity('Pune')
   }
+
+  const handleCick1 = () => {
+    setName("Chandu")
+    setCity("Malvan")
+  }
+
+  useEffect(() =>{
+      console.log(useEffect)
+      document.title = 'You clicked ${name} times.';
+  });
+
+  return(
+    <div className="container">
+
+      <h1 className="h1">
+        Name : {name}
+        <br />
+        City : {city}
+      </h1>
+
+      <button className="b1" onClick = {handleClick}>
+        CHANGE
+      </button>
+      &nbsp;
+      &nbsp;
+      &nbsp;
+
+      <button className="b2" onClick = {handleCick1}>
+        RESET
+      </button>
+
+
+    </div>
+  )
 }
 
 export default App
